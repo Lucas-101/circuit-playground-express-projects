1c5fb99d0bf543d91925c77fe1e142124cbbd8b3
Lucas Lindsey <lucasalindsey@gmail.com>
2020-12-15 10:22:38 -0800

    Update code.py

--- light/code.py
+++ light/code.py
@@ -1,5 +1,8 @@
 from adafruit_circuitplayground import cp
 
+Brightness = 1
+cp.pixels.brightness = Brightness
+
 while True:
     if cp.button_a:
         cp.pixels.fill((255, 255, 255))
@@ -7,6 +10,6 @@ while True:
         pass
     elif cp.button_b:
         cp.pixels.fill((0, 0, 0))
-        
+
         pass
         
