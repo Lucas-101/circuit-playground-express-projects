2d7ecee0d4409746f7f87584aa655ffa97789ac6
Lucas Lindsey <lucasalindsey@gmail.com>
2020-12-15 10:04:26 -0800

    fix code

--- light/code.py
+++ light/code.py
@@ -2,8 +2,11 @@ from adafruit_circuitplayground import cp
 
 while True:
     if cp.button_a:
-            cp.pixels.fill((255, 255, 255))
+        cp.pixels.fill((255, 255, 255))
+
         pass
     elif cp.button_b:
         cp.pixels.fill((0, 0, 0))
+        
         pass
+        
