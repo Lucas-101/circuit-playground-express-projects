8099c40023b8a0649ac8e951495b292d3387219e
Lucas Lindsey <lucasalindsey@gmail.com>
2020-12-22 15:45:01 -0800

    Attempted fix of better candle

--- better_candle/code.py
+++ better_candle/code.py
@@ -4,20 +4,31 @@ import random
 
 is_on = False
 
-while True:
-     if cp.button_a:
-       is_on = True
 
-        pass
-    elif cp.button_b:
+
+while True:
+
+    while is_on:
+        Brightness = random.triangular(0.2, 0.4)
+        cp.pixels.brightness = Brightness
+        cp.pixels.fill((215, 235, 8))
+        cp.pixels.fill((0, 0, 0))
+        time.sleep(0.010)
         
+
+
+    if cp.button_b:
         is_on = False
 
         pass
 
+    elif cp.button_a:
+       is_on = True
 
-while is_on:
-    Brightness = random.triangular(0.2, 0.4)
-    cp.pixels.brightness = Brightness
-    cp.pixels.fill((215, 235, 8))
-    time.sleep(0.010)
+       pass
+
+    
+
+
+
+
